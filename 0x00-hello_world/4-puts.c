4c1faaf7c24d69af9f0ff1da9fab8150b1485603
AkramKhalled <akramkhaled309@gmail.com>
2023-08-11 09:15:59 -0700

    first time with c language

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
  * description: this program prints the next sentence with new line using puts
  * Return: 0
  */
-int main(0)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
