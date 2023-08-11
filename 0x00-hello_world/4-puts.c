5ee3c7b2abac2307f474a419ef3f6fd53450cab3
AkramKhalled <akramkhaled309@gmail.com>
2023-08-11 08:05:15 -0700

    my first c code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - enrty point
+ * description: this program prints the next sentence with new line using puts
+ * return is always 0 as it means success
+ *
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
