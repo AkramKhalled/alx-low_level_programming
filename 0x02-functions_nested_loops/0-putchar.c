358cccd9eff4c0a5143d35d44f0df99d18d0c91e
AkramKhalled <akramkhaled309@gmail.com>
2023-08-15 15:51:04 -0700

    Task_3 C

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: program that prints a word
+ * Return: 0 success
+*/
+int main(void)
+{
+int word;
+
+	printf("_putchar\n", word);
+	return (0);
+}
