fe96753bec247cad5f58185aa32f8bf4bb88bf5f
AkramKhalled <akramkhaled309@gmail.com>
2023-08-14 17:42:41 -0700

    task_2 C

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: Printing lower case letters using putchar
+ * Return: 0 success
+*/
+int main(void)
+{
+	char ch = 'a';
+	char CH = 'A';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		CH++;
+	}
+	putchar('\n');
+	return (0);
+}
